5ef68d959ced0bbf1ed55ba5c16f71c558594f36
Yassine IBN EL HASSAN <yassine.ibnelhassan@outlook.com>
2023-11-29 10:17:01 +0100

    First push

---
+++ src/test/java/com/example/flowdesk/Binance.java
@@ -0,0 +1,185 @@
+package com.example.flowdesk;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.websocket.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@ClientEndpoint
+public class Binance {
+
+    // A map to store the local order book
+    private static Map<Double, Double> orderBook = new ConcurrentHashMap<>();
+
+    // A variable to store the last update id from the snapshot
+    private static long lastUpdateId = 0;
+
+    // A variable to store the previous event's u
+    private static long prevU = 0;
+
+    // A method to get a depth snapshot from the API
+    public static void getDepthSnapshot() {
+        try {
+            // Create a HTTP client and send a GET request
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.binance.com/api/v3/depth?symbol=BNBBTC&limit=5000"))
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Parse the response as a JSON object
+            JSONObject json = new JSONObject(response.body());
+
+            // Get the last update id from the snapshot
+            lastUpdateId = json.getLong("lastUpdateId");
+
+            // Get the bids and asks from the snapshot
+            JSONArray bids = json.getJSONArray("bids");
+            JSONArray asks = json.getJSONArray("asks");
+
+            // Update the local order book with the bids and asks
+            for (int i = 0; i < bids.length(); i++) {
+                JSONArray bid = bids.getJSONArray(i);
+                double price = Double.parseDouble(bid.getString(0));
+                double quantity = bid.getDouble(1);
+                orderBook.put(price, quantity);
+            }
+            for (int i = 0; i < asks.length(); i++) {
+                JSONArray ask = asks.getJSONArray(i);
+                double price = Double.parseDouble(ask.getString(0));
+                double quantity = ask.getDouble(1);
+                orderBook.put(price, quantity);
+            }
+
+            // Print the local order book size
+            System.out.println("Order book size: " + orderBook.size());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // A method to process an event from the stream
+    public static void processEvent(JSONObject event) throws JSONException {
+        // Get the event's U, u, b and a
+        long U = event.getLong("U");
+        long u = event.getLong("u");
+        JSONArray b = event.getJSONArray("b");
+        JSONArray a = event.getJSONArray("a");
+
+        // Check if the event is valid
+        if (u <= lastUpdateId) {
+            // Drop the event
+            System.out.println("Dropped event: " + event);
+            return;
+        }
+        if (prevU == 0) {
+            // The first processed event
+            if (U <= lastUpdateId + 1 && u >= lastUpdateId + 1) {
+                // Valid event
+                System.out.println("First event: " + event);
+            } else {
+                // Invalid event
+                System.out.println("Invalid first event: " + event);
+                return;
+            }
+        } else {
+            // Subsequent events
+            if (U == prevU + 1) {
+                // Valid event
+                System.out.println("Next event: " + event);
+            } else {
+                // Invalid event
+                System.out.println("Invalid next event: " + event);
+                return;
+            }
+        }
+
+        // Update the previous event's u
+        prevU = u;
+
+        // Update the local order book with the event's b and a
+        for (int i = 0; i < b.length(); i++) {
+            JSONArray bid = b.getJSONArray(i);
+            double price = Double.parseDouble(bid.getString(0));
+            double quantity = bid.getDouble(1);
+            if (quantity == 0) {
+                // Remove the price level
+                orderBook.remove(price);
+            } else {
+                // Update the price level
+                orderBook.put(price, quantity);
+            }
+        }
+        for (int i = 0; i < a.length(); i++) {
+            JSONArray ask = a.getJSONArray(i);
+            double price = Double.parseDouble(ask.getString(0));
+            double quantity = ask.getDouble(1);
+            if (quantity == 0) {
+                // Remove the price level
+                orderBook.remove(price);
+            } else {
+                // Update the price level
+                orderBook.put(price, quantity);
+            }
+        }
+
+        // Print the local order book size
+        System.out.println("Order book size: " + orderBook.size());
+    }
+
+    // A method to open a stream to the websocket
+    public static void openStream() {
+        try {
+            // Create a websocket container and connect to the endpoint
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            Session session = container.connectToServer(Binance.class, URI.create("wss://stream.binance.com:9443/ws/bnbbtc@depth"));
+            session.close();
+
+        } catch (IOException | DeploymentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // A method to handle the messages from the stream
+    @OnMessage
+    public void onMessage(String message) throws JSONException {
+        // Parse the message as a JSON object
+        JSONObject event = new JSONObject(message);
+
+        // Process the event
+        processEvent(event);
+    }
+
+    // The main method
+    public static String[][] getOrderBook() {
+        // Get a depth snapshot
+        getDepthSnapshot();
+
+        // Open a stream
+        openStream();
+
+        String[][] orderBookArray = new String[orderBook.size()][2];
+        int i = 0;
+
+        for (Map.Entry<Double, Double> entry : orderBook.entrySet()) {
+            orderBookArray[i][0] = String.valueOf(entry.getKey());
+            orderBookArray[i][1] = String.valueOf(entry.getValue());
+            i++;
+        }
+
+        return orderBookArray;
+    }
+}
---
+++ src/test/java/com/example/flowdesk/DynamicOrderBookComparisonTest.java
@@ -0,0 +1,153 @@
+package com.example.flowdesk;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.flowdesk.Binance.getOrderBook;
+
+public class DynamicOrderBookComparisonTest {
+
+    private static ExtentReports extent;
+    private static ExtentTest test;
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extent-report.html");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        test = extent.createTest("DynamicOrderBookComparison", "Test to compare dynamic order books");
+        // Start a background thread to periodically flush the report
+        Thread flushThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000); // Adjust the interval as needed (e.g., every 30 seconds)
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                extent.flush();
+            }
+        });
+        flushThread.setDaemon(true);
+        flushThread.start();
+
+        // Use WebDriverManager to setup the WebDriver dynamically
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void dynamicOrderBookComparisonTest() {
+        String pageUrl = "https://www.binance.com/fr/trade/BNB_BTC?type=spot";
+        driver.get(pageUrl);
+        String[][] expectedOrderBook;
+        int i = 0;
+
+        while (i < 20) {
+            try {
+                synchronized (DynamicOrderBookComparisonTest.class) {
+                    expectedOrderBook = getOrderBook();
+                }
+
+                if (expectedOrderBook == null) {
+                    test.log(Status.FAIL, "No order book available");
+                    break;
+                }
+
+                compareOrderBooks(driver, expectedOrderBook, "ask");
+                compareOrderBooks(driver, expectedOrderBook, "bid");
+                i++;
+            } catch (Exception e) {
+                test.log(Status.FAIL, "An error occurred: " + e.getMessage());
+            }
+        }
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+
+    private static void compareOrderBooks(WebDriver driver, String[][] expectedOrderBook, String type) {
+        try {
+            // Use WebDriverWait to wait for the order book container to be present
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement orderBookContainer;
+            if (type.equals("ask")) {
+                orderBookContainer = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[@class='orderbook-list-container'])[1]")));
+            } else {
+                orderBookContainer = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[@class='orderbook-list-container'])[2]")));
+            }
+
+            // Extract the order book from the page
+            List<WebElement> orderBookEntries = orderBookContainer.findElements(By.className("row-content"));
+            int totalEntries = orderBookEntries.size();
+            int matchingEntries = 0;
+            for (int i = 0; i < orderBookEntries.size(); i++) {
+                // Compare the order books
+                boolean match = false;
+                WebElement entry = orderBookEntries.get(i);
+                // Use the appropriate CSS selector based on the type (ask or bid)
+                String priceSelector = type.equals("ask") ? ".ask-light" : ".bid-light";
+                double price = Double.parseDouble(entry.findElement(By.cssSelector(priceSelector)).getText());
+                double quantity = Double.parseDouble(entry.findElement(By.cssSelector(".text:nth-child(2)")).getText());
+                for (int j = 0; j < expectedOrderBook.length; j++) {
+                    if (price == Double.parseDouble(expectedOrderBook[j][0]) && quantity == Double.parseDouble(expectedOrderBook[j][1])) {
+                        match = true;
+                        break;
+                    }
+                }
+                if (match) {
+                    matchingEntries++;
+                }
+
+            }
+            float matchingPercentage = (float)matchingEntries / totalEntries * 100;
+            System.out.println("Matching "+type+"s percentage: " + matchingPercentage + "%");
+            if (matchingPercentage<70){
+                test.log(Status.FAIL, "Too many " + type + "s not matching");
+                test.log(Status.INFO, String.valueOf(matchingPercentage + "%"));
+            } else {
+                test.log(Status.PASS, type + "s matching!");
+                test.log(Status.INFO, String.valueOf(matchingPercentage + "%"));
+            }
+
+        } catch (Exception e) {
+            test.log(Status.FAIL, "An error occurred: " + e.getMessage());
+        }
+    }
+
+    private static void printOrderBook(String[][] orderBook) {
+        for (String[] entry : orderBook) {
+            System.out.println("Price: " + entry[0] + ", Quantity: " + entry[1]);
+        }
+    }
+
+    private static void printOrderBookFromPage(List<WebElement> orderBookEntries, String type) {
+        for (WebElement entry : orderBookEntries) {
+            // Use the appropriate CSS selector based on the type (ask or bid)
+            String priceSelector = type.equals("ask") ? ".ask-light" : ".bid-light";
+            String price = entry.findElement(By.cssSelector(priceSelector)).getText();
+            String quantity = entry.findElement(By.cssSelector(".text:nth-child(2)")).getText();
+            System.out.println("Price: " + price + ", Quantity: " + quantity);
+        }
+    }
+}
